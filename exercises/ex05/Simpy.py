5534a5cdf20e477e868e0f9e4d92254c863c3f64
Sadiya Noor <sadiya@email.unc.edu>
2021-04-20 11:08:55 -0400

    ex05

--- exercises/ex05/Simpy.py
+++ exercises/ex05/Simpy.py
@@ -4,10 +4,19 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730411656"
 
 
 class Simpy:
     values: list[float]
 
     # TODO: Your constructor and methods will go here.
+    def __init__(self, x: list[float]):
+        """Constructs a list when given a set of numbers."""
+        self.values = x
+
+    def __repr__(self) -> str:
+        return f"Simpy({self.values})"
+
+    def fill()
+
